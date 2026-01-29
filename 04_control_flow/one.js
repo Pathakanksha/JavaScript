c7b5cf3bea22f1858b034b26b4ea43a4b51b62e8
Akanksha Pathak <pathakakanksha1289@gmail.com>
2026-01-29 20:15:46 +0530

    Add one file

---
+++ 04_control_flow/one.js
@@ -0,0 +1,18 @@
+//if
+
+const isUserLoggedIn = true;
+const temperature = 22;
+if (temperature === 22) {
+    console.log("less than 25");
+}
+else {
+    console.log("more than 25");
+}
+console.log("Execute");
+
+const score= 200;
+
+if (score > 100) {
+    const power= "fly";
+    console.log(`User power: ${power}`);
+    } 
